2be5d0de441d21ab8ab86bf199cb9acb33d16ed0
terasfly <84103159+terasfly@users.noreply.github.com>
2023-03-08 10:19:57 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,96 @@
+let a = '';
+let b = '';
+let sign = '';
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+
+const action = ['-', '+', 'X', '/'];
+
+const out = document.querySelector('.calc-screen p');
+
+const ac = document.querySelector('.ac').addEventListener('click', function() {
+    clearAll()
+})
+
+function clearAll() {
+    out.textContent = 0;
+    a = '';
+    b = '';
+    sign = '';
+
+}
+
+document.querySelector('.buttons').onclick = (event) => {
+
+    if (!event.target.classList.contains('btn')) return;
+    if (event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+
+
+
+    const key = event.target.textContent;
+    console.log(key);
+
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+
+
+            a += key;
+            out.textContent = a;
+            console.log(a);
+
+        } else if (a !== '' && b !== '' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        } else {
+            b += key;
+            out.textContent = b;
+        }
+
+    }
+
+
+
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign)
+        return;
+
+    }
+
+
+
+
+
+    if (key === '=') {
+        if (b === '') b = a;
+        switch (sign) {
+            case '-':
+                a = a - b;
+                break;
+            case '+':
+                a = (+a) + (+b);
+                break;
+            case 'X':
+                a = a * b;
+
+
+                break;
+            case '/':
+                a = a / b;
+                break;
+
+
+        }
+        finish = true;
+        out.textContent = a;
+        console.log(a, b, sign)
+
+    }
+
+
+}
